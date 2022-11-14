b177c30767dfeb50ccb2132f609b12b955c2e365
Gogs <eason@eelic.com>
2022-11-14 10:22:01 +0700

    fix element i18n error

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,30 @@
 <template>
-  <router-view />
+  <div class="app">
+    <el-config-provider :locale="i18nLocale">
+      <router-view></router-view>
+    </el-config-provider>
+  </div>
 </template>
+
+<script setup lang="ts" name="App">
+import { computed } from 'vue';
+import useAppStore from './stores/modules/app';
+import zh from 'element-plus/lib/locale/lang/zh-cn'
+import en from 'element-plus/lib/locale/lang/en' 
+import id from 'element-plus/lib/locale/lang/id' 
+
+const langStore = useAppStore();
+
+// element select lang setting 语言配置
+const i18nLocale = computed(() => {
+	if (langStore.lang == "zh") return zh;
+	if (langStore.lang == "en") return en;
+  if (langStore.lang == "id") return id;
+});
+
+</script>
+<style scoped>
+.app {
+  height: 100%;
+}
+</style>
