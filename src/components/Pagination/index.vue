21e419dba2dfe07e8a0e4fadc2c22d9a2bb44998
Gogs <eason@eelic.com>
2022-11-24 18:21:33 +0700

    ts standard pagination

--- src/components/Pagination/index.vue
+++ src/components/Pagination/index.vue
@@ -14,53 +14,41 @@
   </div>
 </template>
 
-<script setup lang="ts" name="Pageination">
+<script lang="ts" setup name="Pageination">
 import { scrollTo } from '@/utils/scroll-to'
 import { computed } from 'vue';
+interface Props {
+  total: number; // required
+  page?: number;
+  limit?: number;
+  pageSizes?: number[];
+  pagerCount?: number;
+  layout?: string;
+  background?: boolean;
+  autoScroll?: boolean;
+  hidden?: boolean;
+};
 
-const props = defineProps({
-  total: {
-    required: true,
-    type: Number
-  },
-  page: {
-    type: Number,
-    default: 1
-  },
-  limit: {
-    type: Number,
-    default: 20
-  },
-  pageSizes: {
-    type: Array,
-    default() {
-      return [10, 20, 30, 50]
-    }
-  },
+const props = withDefaults(defineProps<Props>(), {
+  total: 0,
+  page: 1,
+  limit: 20,
+  pageSizes: ()=>[10, 20, 30, 50],
   // 移动端页码按钮的数量端默认值5
-  pagerCount: {
-    type: Number,
-    default: document.body.clientWidth < 992 ? 5 : 7
-  },
-  layout: {
-    type: String,
-    default: 'total, sizes, prev, pager, next, jumper'
-  },
-  background: {
-    type: Boolean,
-    default: true
-  },
-  autoScroll: {
-    type: Boolean,
-    default: true
-  },
-  hidden: {
-    type: Boolean,
-    default: false
-  }
+  pagerCount: document.body.clientWidth < 992 ? 5 : 7,
+  layout: 'total, sizes, prev, pager, next, jumper',
+  background: true,
+  autoScroll: true,
+  hidden: false
 });
 
-const emit = defineEmits(['update:page','update:limit','pagination']);
+interface Emit{
+  (e: 'update:page', page: number): void; 
+  (e: 'update:limit', limit: number): void;  
+  (e: 'pagination', { page, limit }): void;  
+}
+const emit = defineEmits<Emit>()
+// to-do: check again https://www.mybj123.com/17272.html
 const currentPage = computed({
   get() {
     return props.page
@@ -77,6 +65,7 @@ const pageSize = computed({
     emit('update:limit', val)
   }
 })
+
 function handleSizeChange(val: number) {
   if (currentPage.value * val > props.total) {
     currentPage.value = 1
