43d8729402dd4dac428afc4426247f41ca4fb395
Deva <deva@eelic.com>
2023-01-31 15:32:33 +0700

    make monitor online i18n

--- src/views/monitor/online/index.vue
+++ src/views/monitor/online/index.vue
@@ -1,19 +1,19 @@
 <template>
    <div class="app-container">
       <el-form :model="queryParams" ref="queryRef" :inline="true">
-         <el-form-item label="登录地址" prop="ipaddr">
+         <el-form-item :label="$t('loginInfor.loginAddress')" prop="ipaddr">
             <el-input
                v-model="queryParams.ipaddr"
-               placeholder="请输入登录地址"
+               :placeholder="$t('loginInfor.loginAddressPlaceholder')"
                clearable
                style="width: 200px"
                @keyup.enter="handleQuery"
             />
          </el-form-item>
-         <el-form-item label="用户名称" prop="userName">
+         <el-form-item :label="$t('user.name')" prop="userName">
             <el-input
                v-model="queryParams.userName"
-               placeholder="请输入用户名称"
+               :placeholder="$t('user.UserPlaceholder')"
                clearable
                style="width: 200px"
                @keyup.enter="handleQuery"
@@ -29,24 +29,24 @@
          :data="onlineList.slice((pageNum - 1) * pageSize, pageNum * pageSize)"
          style="width: 100%;"
       >
-         <el-table-column label="序号" width="50" type="index" align="center">
+         <el-table-column :label="$t('user.name')" width="50" type="index" align="center">
             <template #default="scope">
                <span>{{ (pageNum - 1) * pageSize + scope.$index + 1 }}</span>
             </template>
          </el-table-column>
-         <el-table-column label="会话编号" align="center" prop="tokenId" :show-overflow-tooltip="true" />
-         <el-table-column label="登录名称" align="center" prop="userName" :show-overflow-tooltip="true" />
-         <el-table-column label="所属部门" align="center" prop="deptName" :show-overflow-tooltip="true" />
-         <el-table-column label="主机" align="center" prop="ipaddr" :show-overflow-tooltip="true" />
-         <el-table-column label="登录地点" align="center" prop="loginLocation" :show-overflow-tooltip="true" />
-         <el-table-column label="操作系统" align="center" prop="os" :show-overflow-tooltip="true" />
-         <el-table-column label="浏览器" align="center" prop="browser" :show-overflow-tooltip="true" />
-         <el-table-column label="登录时间" align="center" prop="loginTime" width="180">
+         <el-table-column :label="$t('online.sessionNumber')" align="center" prop="tokenId" :show-overflow-tooltip="true" />
+         <el-table-column :label="$t('online.loginName')" align="center" prop="userName" :show-overflow-tooltip="true" />
+         <el-table-column :label="$t('user.departementName')" align="center" prop="deptName" :show-overflow-tooltip="true" />
+         <el-table-column :label="$t('online.host')" align="center" prop="ipaddr" :show-overflow-tooltip="true" />
+         <el-table-column :label="$t('loginInfor.loginLocation')" align="center" prop="loginLocation" :show-overflow-tooltip="true" />
+         <el-table-column :label="$t('loginInfor.os')" align="center" prop="os" :show-overflow-tooltip="true" />
+         <el-table-column :label="$t('loginInfor.browser')" align="center" prop="browser" :show-overflow-tooltip="true" />
+         <el-table-column :label="$t('loginInfor.loginTime')" align="center" prop="loginTime" width="180">
             <template #default="scope">
                <span>{{ parseTime(scope.row.loginTime) }}</span>
             </template>
          </el-table-column>
-         <el-table-column label="操作" align="center" class-name="small-padding fixed-width">
+         <el-table-column :label="$t('user.operate')" align="center" class-name="small-padding fixed-width">
             <template #default="scope">
                <el-button
                   type="primary"
@@ -54,7 +54,7 @@
                   @click="handleForceLogout(scope.row)"
                   link
                   v-hasPermi="['monitor:online:forceLogout']"
-               >强退</el-button>
+               >{{ $t('online.forceLogout') }}</el-button>
             </template>
          </el-table-column>
       </el-table>
@@ -63,11 +63,14 @@
    </div>
 </template>
 
-<script setup lang="ts" name="Online">
+<script setup lang="ts" nam
+e="Online">
 import { forceLogout, list as initData } from "@/api/monitor/online";
 import { ComponentInternalInstance, getCurrentInstance, ref } from "vue";
 import type { ElForm } from "element-plus";
+import i18n from '@/lang/index';
 
+const {t} = i18n.global;  
 const { proxy } = getCurrentInstance() as ComponentInternalInstance;
 
 const onlineList = ref([]);
@@ -103,11 +106,11 @@ function resetQuery() {
 }
 /** 强退按钮操作 */
 function handleForceLogout(row) {
-    proxy?.$modal.confirm('是否确认强退名称为"' + row.userName + '"的用户?').then(function () {
+    proxy?.$modal.confirm(t('online.confirm1') + row.userName + t('online.confirm2')).then(function () {
   return forceLogout(row.tokenId);
   }).then(() => {
     getList();
-    proxy.$modal.msgSuccess("删除成功");
+    proxy.$modal.msgSuccess(t('user.succesDeleted'));
   }).catch(() => {});
 }
 
