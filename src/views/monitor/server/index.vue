4f655cde614dceb9a2c988c5000a9f629d31adff
Deva <deva@eelic.com>
2023-02-01 13:19:48 +0700

    make i18n monitor server

--- src/views/monitor/server/index.vue
+++ src/views/monitor/server/index.vue
@@ -8,25 +8,25 @@
             <table cellspacing="0" style="width: 100%;">
               <thead>
                 <tr>
-                  <th class="el-table__cell is-leaf"><div class="cell">属性</div></th>
-                  <th class="el-table__cell is-leaf"><div class="cell">值</div></th>
+                  <th class="el-table__cell is-leaf"><div class="cell">{{ $t('server.attributes') }}</div></th>
+                  <th class="el-table__cell is-leaf"><div class="cell">{{ $t('genTable.yAxisChart') }}</div></th>
                 </tr>
               </thead>
               <tbody>
                 <tr>
-                  <td class="el-table__cell is-leaf"><div class="cell">核心数</div></td>
+                  <td class="el-table__cell is-leaf"><div class="cell">{{ $t('server.coreNumber') }}</div></td>
                   <td class="el-table__cell is-leaf"><div class="cell" v-if="server.cpu">{{ server.cpu.cpuNum }}</div></td>
                 </tr>
                 <tr>
-                  <td class="el-table__cell is-leaf"><div class="cell">用户使用率</div></td>
+                  <td class="el-table__cell is-leaf"><div class="cell">{{ $t('server.userUtilizationRate') }}</div></td>
                   <td class="el-table__cell is-leaf"><div class="cell" v-if="server.cpu">{{ server.cpu.used }}%</div></td>
                 </tr>
                 <tr>
-                  <td class="el-table__cell is-leaf"><div class="cell">系统使用率</div></td>
+                  <td class="el-table__cell is-leaf"><div class="cell">{{ $t('server.systemUsage') }}</div></td>
                   <td class="el-table__cell is-leaf"><div class="cell" v-if="server.cpu">{{ server.cpu.sys }}%</div></td>
                 </tr>
                 <tr>
-                  <td class="el-table__cell is-leaf"><div class="cell">当前空闲率</div></td>
+                  <td class="el-table__cell is-leaf"><div class="cell">{{ $t('server.currentAvailability') }}</div></td>
                   <td class="el-table__cell is-leaf"><div class="cell" v-if="server.cpu">{{ server.cpu.free }}%</div></td>
                 </tr>
               </tbody>
@@ -37,34 +37,34 @@
 
       <el-col :span="12" class="card-box">
         <el-card>
-          <template #header><span>内存</span></template>
+          <template #header><span>{{ $t('server.memory') }}</span></template>
           <div class="el-table el-table--enable-row-hover el-table--medium">
             <table cellspacing="0" style="width: 100%;">
               <thead>
                 <tr>
-                  <th class="el-table__cell is-leaf"><div class="cell">属性</div></th>
-                  <th class="el-table__cell is-leaf"><div class="cell">内存</div></th>
+                  <th class="el-table__cell is-leaf"><div class="cell">{{ $t('server.attributes') }}</div></th>
+                  <th class="el-table__cell is-leaf"><div class="cell">{{ $t('server.memory') }}</div></th>
                   <th class="el-table__cell is-leaf"><div class="cell">JVM</div></th>
                 </tr>
               </thead>
               <tbody>
                 <tr>
-                  <td class="el-table__cell is-leaf"><div class="cell">总内存</div></td>
+                  <td class="el-table__cell is-leaf"><div class="cell">{{ $t('server.memoryTotal') }}</div></td>
                   <td class="el-table__cell is-leaf"><div class="cell" v-if="server.mem">{{ server.mem.total }}G</div></td>
                   <td class="el-table__cell is-leaf"><div class="cell" v-if="server.jvm">{{ server.jvm.total }}M</div></td>
                 </tr>
                 <tr>
-                  <td class="el-table__cell is-leaf"><div class="cell">已用内存</div></td>
+                  <td class="el-table__cell is-leaf"><div class="cell">{{ $t('server.usedMemory') }}</div></td>
                   <td class="el-table__cell is-leaf"><div class="cell" v-if="server.mem">{{ server.mem.used}}G</div></td>
                   <td class="el-table__cell is-leaf"><div class="cell" v-if="server.jvm">{{ server.jvm.used}}M</div></td>
                 </tr>
                 <tr>
-                  <td class="el-table__cell is-leaf"><div class="cell">剩余内存</div></td>
+                  <td class="el-table__cell is-leaf"><div class="cell">{{ $t('server.remainingMemory') }}</div></td>
                   <td class="el-table__cell is-leaf"><div class="cell" v-if="server.mem">{{ server.mem.free }}G</div></td>
                   <td class="el-table__cell is-leaf"><div class="cell" v-if="server.jvm">{{ server.jvm.free }}M</div></td>
                 </tr>
                 <tr>
-                  <td class="el-table__cell is-leaf"><div class="cell">使用率</div></td>
+                  <td class="el-table__cell is-leaf"><div class="cell">{{ $t('server.utilizationRate') }}</div></td>
                   <td class="el-table__cell is-leaf"><div class="cell" v-if="server.mem" :class="{'text-danger': server.mem.usage > 80}">{{ server.mem.usage }}%</div></td>
                   <td class="el-table__cell is-leaf"><div class="cell" v-if="server.jvm" :class="{'text-danger': server.jvm.usage > 80}">{{ server.jvm.usage }}%</div></td>
                 </tr>
@@ -76,20 +76,20 @@
 
       <el-col :span="24" class="card-box">
         <el-card>
-          <template #header><span>服务器信息</span></template>
+          <template #header><span>{{ $t('server.serverInfo') }}</span></template>
           <div class="el-table el-table--enable-row-hover el-table--medium">
             <table cellspacing="0" style="width: 100%;">
               <tbody>
                 <tr>
-                  <td class="el-table__cell is-leaf"><div class="cell">服务器名称</div></td>
+                  <td class="el-table__cell is-leaf"><div class="cell">{{ $t('server.serverName') }}</div></td>
                   <td class="el-table__cell is-leaf"><div class="cell" v-if="server.sys">{{ server.sys.computerName }}</div></td>
-                  <td class="el-table__cell is-leaf"><div class="cell">操作系统</div></td>
+                  <td class="el-table__cell is-leaf"><div class="cell">{{ $t('loginInfor.os') }}</div></td>
                   <td class="el-table__cell is-leaf"><div class="cell" v-if="server.sys">{{ server.sys.osName }}</div></td>
                 </tr>
                 <tr>
-                  <td class="el-table__cell is-leaf"><div class="cell">服务器IP</div></td>
+                  <td class="el-table__cell is-leaf"><div class="cell">{{ $t('server.serverIP') }}</div></td>
                   <td class="el-table__cell is-leaf"><div class="cell" v-if="server.sys">{{ server.sys.computerIp }}</div></td>
-                  <td class="el-table__cell is-leaf"><div class="cell">系统架构</div></td>
+                  <td class="el-table__cell is-leaf"><div class="cell">{{ $t('server.systemStructure') }}</div></td>
                   <td class="el-table__cell is-leaf"><div class="cell" v-if="server.sys">{{ server.sys.osArch }}</div></td>
                 </tr>
               </tbody>
@@ -100,32 +100,32 @@
 
       <el-col :span="24" class="card-box">
         <el-card>
-          <template #header><span>Java虚拟机信息</span></template>
+          <template #header><span>{{ $t('server.JVMInfo') }}</span></template>
           <div class="el-table el-table--enable-row-hover el-table--medium">
             <table cellspacing="0" style="width: 100%;table-layout:fixed;">
               <tbody>
                 <tr>
-                  <td class="el-table__cell is-leaf"><div class="cell">Java名称</div></td>
+                  <td class="el-table__cell is-leaf"><div class="cell">{{ $t('server.javaName') }}</div></td>
                   <td class="el-table__cell is-leaf"><div class="cell" v-if="server.jvm">{{ server.jvm.name }}</div></td>
-                  <td class="el-table__cell is-leaf"><div class="cell">Java版本</div></td>
+                  <td class="el-table__cell is-leaf"><div class="cell">{{ $t('server.javaVersion') }}</div></td>
                   <td class="el-table__cell is-leaf"><div class="cell" v-if="server.jvm">{{ server.jvm.version }}</div></td>
                 </tr>
                 <tr>
-                  <td class="el-table__cell is-leaf"><div class="cell">启动时间</div></td>
+                  <td class="el-table__cell is-leaf"><div class="cell">{{ $t('server.startTime') }}</div></td>
                   <td class="el-table__cell is-leaf"><div class="cell" v-if="server.jvm">{{ server.jvm.startTime }}</div></td>
-                  <td class="el-table__cell is-leaf"><div class="cell">运行时长</div></td>
+                  <td class="el-table__cell is-leaf"><div class="cell">{{ $t('server.runTime') }}</div></td>
                   <td class="el-table__cell is-leaf"><div class="cell" v-if="server.jvm">{{ server.jvm.runTime }}</div></td>
                 </tr>
                 <tr>
-                  <td colspan="1" class="el-table__cell is-leaf"><div class="cell">安装路径</div></td>
+                  <td colspan="1" class="el-table__cell is-leaf"><div class="cell">{{ $t('server.installationPath') }}</div></td>
                   <td colspan="3" class="el-table__cell is-leaf"><div class="cell" v-if="server.jvm">{{ server.jvm.home }}</div></td>
                 </tr>
                 <tr>
-                  <td colspan="1" class="el-table__cell is-leaf"><div class="cell">项目路径</div></td>
+                  <td colspan="1" class="el-table__cell is-leaf"><div class="cell">{{ $t('server.projectPath') }}</div></td>
                   <td colspan="3" class="el-table__cell is-leaf"><div class="cell" v-if="server.sys">{{ server.sys.userDir }}</div></td>
                 </tr>
                 <tr>
-                  <td colspan="1" class="el-table__cell is-leaf"><div class="cell">运行参数</div></td>
+                  <td colspan="1" class="el-table__cell is-leaf"><div class="cell">{{ $t('server.operatingParameters') }}</div></td>
                   <td colspan="3" class="el-table__cell is-leaf"><div class="cell" v-if="server.jvm">{{ server.jvm.inputArgs }}</div></td>
                 </tr>
               </tbody>
@@ -136,18 +136,18 @@
 
       <el-col :span="24" class="card-box">
         <el-card>
-          <template #header><span>磁盘状态</span></template>
+          <template #header><span>{{ $t('server.diskStatus') }}</span></template>
           <div class="el-table el-table--enable-row-hover el-table--medium">
             <table cellspacing="0" style="width: 100%;">
               <thead>
                 <tr>
-                  <th class="el-table__cell el-table__cell is-leaf"><div class="cell">盘符路径</div></th>
-                  <th class="el-table__cell is-leaf"><div class="cell">文件系统</div></th>
-                  <th class="el-table__cell is-leaf"><div class="cell">盘符类型</div></th>
-                  <th class="el-table__cell is-leaf"><div class="cell">总大小</div></th>
-                  <th class="el-table__cell is-leaf"><div class="cell">可用大小</div></th>
-                  <th class="el-table__cell is-leaf"><div class="cell">已用大小</div></th>
-                  <th class="el-table__cell is-leaf"><div class="cell">已用百分比</div></th>
+                  <th class="el-table__cell el-table__cell is-leaf"><div class="cell">{{ $t('server.drivePath') }}</div></th>
+                  <th class="el-table__cell is-leaf"><div class="cell">{{ $t('server.fileSystem') }}</div></th>
+                  <th class="el-table__cell is-leaf"><div class="cell">{{ $t('server.fileSystem') }}</div></th>
+                  <th class="el-table__cell is-leaf"><div class="cell">{{ $t('server.totalSize') }}</div></th>
+                  <th class="el-table__cell is-leaf"><div class="cell">{{ $t('server.availableSize') }}</div></th>
+                  <th class="el-table__cell is-leaf"><div class="cell">{{ $t('server.usedSize') }}</div></th>
+                  <th class="el-table__cell is-leaf"><div class="cell">{{ $t('server.precentageUsed') }}</div></th>
                 </tr>
               </thead>
               <tbody v-if="server.sysFiles">
@@ -172,6 +172,7 @@
 <script setup lang="ts" >
 import { getServer } from '@/api/monitor/server';
 import { ComponentInternalInstance, getCurrentInstance, ref } from "vue";
+import i18n from '@/lang/index';
 
 interface Cpu {
   cpuNum: number;
@@ -229,12 +230,13 @@ interface server {
   sysFiles: SysFiles[];
 }
 
+const {t} = i18n.global;
 const server = ref({} as server);
 const { proxy } = getCurrentInstance() as ComponentInternalInstance;
 
 
 function getList() {
-  proxy?.$modal.loading("正在加载服务监控数据，请稍候！");
+  proxy?.$modal.loading(t('server.loading'));
   getServer().then(response => {
     server.value = response.data;
     proxy?.$modal.closeLoading();
